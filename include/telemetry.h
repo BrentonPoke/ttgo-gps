61983e09fc4bafd723cd1e532f7c384bb97e92ad
Brenton Poke <brentonpoke@outlook.com>
2022-11-24 15:04:19 -0500

    syncing stuff from campus
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>

--- include/telemetry.h
+++ include/telemetry.h
@@ -6,23 +6,12 @@
 #include <coords.h>
 #include <TinyGPS++.h>
 #include <ArduinoJson.h>
+#include <ctime>
 
-std::string deviceId("MAMAGPS6");
+std::string deviceId("MAMAGPSE");
 TinyGPSPlus tgps;
 HardwareSerial GPS(1);
-
-std::time_t tmConvert_t(int YYYY, int MM, byte DD, byte hh, byte mm, byte ss)
-{
-    std::tm tmSet{};
-    tmSet.tm_year = YYYY - 1900;
-    tmSet.tm_mon = MM - 1;
-    tmSet.tm_mday = DD;
-    tmSet.tm_hour = hh;
-    tmSet.tm_min = mm;
-    tmSet.tm_sec = ss;
-    std::time_t t = std::mktime(&tmSet);
-    return mktime(std::gmtime(&t));
-}
+std::time_t tmConvert_t(int YYYY, byte MM, byte DD, byte hh, byte mm, byte ss);
 
 // Getting GPS data
 String getGPSData(std::pair<double,double> gpsPair, byte* seqid, int count) {
@@ -106,6 +95,19 @@ String getGPSData(std::pair<double,double> gpsPair, byte* seqid, int count) {
     }
 
     return jsonstat;
+}
+
+std::time_t tmConvert_t(int YYYY, byte MM, byte DD, byte hh, byte mm, byte ss)
+{
+    std::tm tmSet{};
+    tmSet.tm_year = YYYY - 1900;
+    tmSet.tm_mon = MM - 1;
+    tmSet.tm_mday = DD;
+    tmSet.tm_hour = hh;
+    tmSet.tm_min = mm;
+    tmSet.tm_sec = ss;
+    std::time_t t = std::mktime(&tmSet);
+    return mktime(std::gmtime(&t));
 }
 
 #endif TELEMETRY_H
