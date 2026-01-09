3e3218b689be188cb11da721faad8eeb1c9dfc84
Brenton Poke <brentonpoke@outlook.com>
2026-01-09 13:13:42 -0500

    setting up new code
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>

--- include/telemetry.h
+++ include/telemetry.h
@@ -3,71 +3,59 @@
 //
 #ifndef TELEMETRY_H
 #define TELEMETRY_H
-#include <coords.h>
-#include <TinyGPS++.h>
+//For random coordinate generation
+#include <iostream>
+#include <random>
+#include <utility>
+#include <cmath>
+
+#include <DuckGPS.h>
 #include <ArduinoJson.h>
 #include <ctime>
+#include <Adafruit_SSD1306.h>
+//#include <XPowersLib.h>
+#include <utils/DuckUtils.h>
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+#define OLED_RESET     -1
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_ADDRESS 0x3c
 
-std::string deviceId("MAMAGPSE");
-TinyGPSPlus tgps;
-HardwareSerial GPS(1);
-std::time_t tmConvert_t(int YYYY, byte MM, byte DD, byte hh, byte mm, byte ss);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+std::string deviceId("MAMAGPSB");
+DuckGPS tgps;
+//XPowersPMU axp;
 
 // Getting GPS data
-String getGPSData(std::pair<double,double> gpsPair, std::array<uint8_t,6>& seqid, int count) {
-
-    // Printing the GPS data
-    Serial.println("--- GPS ---");
-    Serial.print("Latitude  : ");
-    Serial.println(tgps.location.lat(), 5);
-    Serial.print("Longitude : ");
-    Serial.println(tgps.location.lng(), 4);
-    Serial.print("Altitude  : ");
-    Serial.print(tgps.altitude.meters());
-    Serial.println("M");
-    Serial.print("Satellites: ");
-    Serial.println(tgps.satellites.value());
-    Serial.print("Raw Date  : ");
-    Serial.println(tgps.date.value());
-    Serial.print("Epoch     : ");
-    Serial.println(tmConvert_t(
-            tgps.date.year(),
-            tgps.date.month(),
-            tgps.date.day(),
-            tgps.time.hour(),
-            tgps.time.minute(),
-            tgps.time.second()));
-    Serial.print("Speed     : ");
-    Serial.println(tgps.speed.kmph());
-    Serial.println("**********************");
+std::string getGPSData(std::pair<float,float> gpsPair, std::array<uint8_t,6>& seqid, int count) {
 
     //test of EMS ideas...
 
     std::string arr[3] = {"NB","M","W"};
-    std::string str(seqid.begin(), seqid.end());
-    DynamicJsonDocument nestdoc(229);
-    JsonObject ems  = nestdoc.createNestedObject("EMS");
+    // not sure why I need to do this now
+
+    JsonDocument nestdoc;
+    JsonObject ems  = nestdoc["EMS"].to<JsonObject>();
     ems["G"] = arr[esp_random() % 3];
     ems["Device"] = deviceId;
-    ems["seqID"] = str;
+    ems["seqID"] = duckutils::toString(seqid);
     ems["seqNum"] = count;
+   // ems["Voltage"] = axp.getBattVoltage();
+   // ems["level"] = axp.getBatteryPercent();
     ems["GPS"]["lon"] = gpsPair.first;
     ems["GPS"]["lat"] = gpsPair.second;
-    ems["GPS"]["satellites"] = tgps.satellites.value();
-    ems["GPS"]["time"] = tmConvert_t(
-            tgps.date.year(),
-            tgps.date.month(),
-            tgps.date.day(),
-            tgps.time.hour(),
-            tgps.time.minute(),
-            tgps.time.second());
-    ems["GPS"]["alt"] = tgps.altitude.meters();
-    ems["GPS"]["speed"] = tgps.speed.kmph();
+    ems["GPS"]["satellites"] = tgps.satellites();
+    ems["GPS"]["time"] = tgps.epoch();
+    ems["GPS"]["alt"] = tgps.altitude(DuckGPS::AltitudeUnit::meter);
+    ems["GPS"]["speed"] = tgps.speed(DuckGPS::SpeedUnit::kmph);
 
-    String jsonstat;
+    std::string jsonstat;
     serializeJson(ems,jsonstat);
 
-    Serial.println("Payload: " + jsonstat);
+    Serial.print("Payload: ");
+    Serial.println(jsonstat.c_str());
     Serial.print("Payload Size: ");
     Serial.println(jsonstat.length());
 
@@ -75,39 +63,49 @@ String getGPSData(std::pair<double,double> gpsPair, std::array<uint8_t,6>& seqid
     display.setTextSize(1); // Draw 2X-scale text
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
-    display.println("Generated Message");
     display.println(deviceId.c_str());
     display.print("SeqID: ");
-    display.println(ems["seqID"].as<String>());
+    display.println(ems["seqID"].as<std::string>().c_str());
     display.print("Time: ");
     display.println(ems["GPS"]["time"].as<long>());
+    display.print("Satellites: ");
+    display.println(ems["GPS"]["satellites"].as<int>());
+    display.println("Coordidnates:");
+    display.print(ems["GPS"]["lat"].as<float>(), 6);
+    display.print(", ");
+    display.println(ems["GPS"]["lon"].as<float>(), 6);
+    display.print("Voltage: ");
+   // display.println(axp.getBattVoltage());
+    display.print("Percentage: ");
+   // display.println(axp.getBatteryPercent());
     display.display();
 
     /*
      char buff[229];
     unishox2_compress_simple(jsonstat.c_str(), int(jsonstat.length()), buff);
      */
-    // Creating a message of the Latitude and Longitude
-    // Check to see if GPS data is being received
-    if (millis() > 5000 && tgps.charsProcessed() < 10)
-    {
-        Serial.println(F("No GPS data received: check wiring"));
-    }
 
     return jsonstat;
 }
 
-std::time_t tmConvert_t(int YYYY, byte MM, byte DD, byte hh, byte mm, byte ss)
-{
-    std::tm tmSet{};
-    tmSet.tm_year = YYYY - 1900;
-    tmSet.tm_mon = MM - 1;
-    tmSet.tm_mday = DD;
-    tmSet.tm_hour = hh;
-    tmSet.tm_min = mm;
-    tmSet.tm_sec = ss;
-    std::time_t t = std::mktime(&tmSet);
-    return mktime(std::gmtime(&t));
-}
+std::pair<float, float> getLocation(float x0, float y0, int radius) {
+    std::random_device rd;
+    std::uniform_int_distribution<int> dist(0,49);
+    // Convert radius from meters to degrees
+    float radiusInDegrees = radius / 111000.0f;
 
-#endif TELEMETRY_H
+    float u = dist(rd);
+    float v = dist(rd);
+    float w = radiusInDegrees * sqrt(u);
+    float t = 2 * M_PI * v;
+    float x = w * cos(t);
+    float y = w * sin(t);
+
+    // Adjust the x-coordinate for the shrinking of the east-west distances
+    float new_x = x / cos(M_PI * y0);
+    float foundLongitude = new_x + x0;
+    float foundLatitude = y + y0;
+    //std::cout << "Longitude: " ;
+    return std::make_pair(foundLongitude, foundLatitude);
+}
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,48 @@
 #include <string>
+#include <algorithm>
+#include <array>
 #include <arduino-timer.h>
-#include <MamaDuck.h>
+#include "Ducks/MamaDuck.h"
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_SSD1306.h>
+#include <random>
+#include <telemetry.h>
 
 #ifdef SERIAL_PORT_USBVIRTUAL
 #define Serial SERIAL_PORT_USBVIRTUAL
 #endif
-#define CDP_LOG_INFO
-#define ARDUINO_TBeam
-//GPS
-#define LORA_FREQ 915.0 // Frequency Range. Set for US Region 915.0Mhz
-#define LORA_TXPOWER 20 // Transmit Power
-// LORA HELTEC PIN CONFIG
-#define LORA_CS_PIN 18
-#define LORA_DIO0_PIN 26
-#define LORA_DIO1_PIN -1 // unused
-#define LORA_RST_PIN 23
 const int btnPin = 37;
-MamaDuck duck;
+MamaDuck duck(deviceId);
 
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
-#define OLED_RESET     -1
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define SCREEN_ADDRESS 0x3c
-#define CDPCFG_WIFI_NONE
-
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#include <telemetry.h>
+std::mt19937 gen;
 
 auto timer = timer_create_default();
 const int INTERVAL_MS = 20000;
-//int counter = 1;
-
-static void smartDelay(unsigned long ms)
-{
-    unsigned long start = millis();
-    do
-    {
-        while (GPS.available())
-            tgps.encode(GPS.read());
-    } while (millis() - start < ms);
-}
-
 
 bool runSensor(void*) {
     // Encoding the GPS
-    smartDelay(10000);
+    tgps.readData(10000);
     //make sure there is at least one point generated
-    std::pair<double,double> gpsPair = getLocation(tgps.location.lng(),tgps.location.lat(),15);
+    std::pair<float,float> gpsPair = getLocation(tgps.lng(),tgps.lat(),15);
     //a max of 3 packets per transmission session
-    unsigned int count = esp_random() % 3;
+
     std::array<uint8_t,6> seqID;
     std::uniform_int_distribution<int> distribution(0,35);
-    std::default_random_engine eng{esp_random()};
     std::string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     for (int i = 0; i < seqID.size() ; i++) {
-        seqID[i] = digits[distribution(eng)];
+        seqID[i] = digits[distribution(gen)];
     }
     //make sure at least one packet is sent
     for (int i = 0; i <= 3; i++) {
         std::string sensorVal = getGPSData(gpsPair, seqID, i);
         //Serial.println(sensorVal);
         //Send gps data
-        duck.sendData(topics::location, sensorVal);
+        duck.sendData(topics::gps, sensorVal);
         //counter++;
         delay(1000);
     }
+
     return true;
 }
 
@@ -81,35 +52,32 @@ void setup() {
     // setup the duck NOTE: The Device ID must be exactly 8 bytes otherwise it
     // will get rejected
     Wire.begin(21, 22);
-    WiFi.mode(WIFI_OFF);
-    std::vector<byte> devId;
-    devId.insert(devId.end(), deviceId.begin(), deviceId.end());
-
     // Use the default setup provided by the SDK
-    duck.setDeviceId(devId);
-    // initialize the serial component with the hardware supported baudrate
-    duck.setupSerial(115200);
-    duck.setupRadio(LORA_FREQ, LORA_CS_PIN, LORA_RST_PIN, LORA_DIO0_PIN, LORA_DIO1_PIN, LORA_TXPOWER);
+
+
     Serial.println("MAMA-DUCK...READY!");
 
     pinMode(btnPin, INPUT);
-    GPS.begin(9600, SERIAL_8N1, 34, 12);   //17-TX 18-RX
 
     display.begin(SSD1306_SWITCHCAPVCC,SCREEN_ADDRESS);
     display.display();
+    tgps.setup();
+    gen.seed(analogRead(0));
+    duck.setupWithDefaults();
 
-    // initialize the timer. The timer thread runs separately from the main loop
-    // and will trigger sending a counter message.
-    //timer.every(INTERVAL_MS, runSensor);
-    runSensor(new void*);
+    runSensor(nullptr);
 }
 
 void loop() {
     timer.tick();
-    std::mt19937 gen(millis());
+
     std::uniform_int_distribution<> distrib(1000, 300000);
-        if (timer.empty())
-            timer.in(distrib(gen), runSensor);
+//    auto now = duckutils::Timer::now();
+//    auto time = now + std::chrono::milliseconds(distrib(gen));
+//    std::cout << "Next sensor read in " << std::chrono::duration_cast<std::chrono::seconds>(time - now).count() << " seconds." << std::endl;
+//    duckutils::Timer(time,runSensor, nullptr);
+
+    if (timer.empty()) timer.in(distrib(gen), runSensor);
     // Use the default run(). The Mama duck is designed to also forward data it receives
     // from other ducks, across the network. It has a basic routing mechanism built-in
     // to prevent messages from hoping endlessly.
